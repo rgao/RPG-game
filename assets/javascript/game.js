7733292d30e2f67042138024d026ee2a97bf9f22
rgao <xinghoshi@gmail.com>
2018-09-18 13:33:55 -0700

    images, wireframe, squirrel squirrel objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    var fox = {
+        role: "none",
+        hp: 180,
+        ctratk: 15,
+    };
+
+    var gray = {
+        role: "none",
+        hp: 150,
+        ctratk: 17,
+    };
+
+    var red = {
+        role: "none",
+        hp: 120,
+        ctratk: 12,
+    };
+
+    var chipmunk = {
+        role: "none",
+        hp: 100,
+        ctratk: 20,
+    };
+
+    var squirrels = [fox, gray, red, chipmunk];
+})
